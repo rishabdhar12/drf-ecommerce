63670bcabbde6be031c8a9eaffd938446b59cee7
nanodano <rishabdhar12@gmail.com>
2023-12-19 01:06:27 +0530

    models

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.SET)
