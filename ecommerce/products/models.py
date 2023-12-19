43396be07bdcdc28e1d958fda71e8183a15fb59f
nanodano <rishabdhar12@gmail.com>
2023-12-19 23:18:57 +0530

    models

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,16 +1,33 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)  # Nested relation
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     is_digital = models.BooleanField(default=False)
-    brand = models.ForeignKey(Brand, on_delete=models.SET)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    # Nested Relation
+    category = TreeForeignKey("Category", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
