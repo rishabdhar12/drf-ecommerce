9f139f0f8a6c5cfdea3903643a1e8f9b6cce2c15
nanodano <rishabdhar12@gmail.com>
2023-12-20 00:53:03 +0530

    views, serializers

---
+++ ecommerce/products/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Brand, Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand 
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product 
+        fields = "__all__"
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryView(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
